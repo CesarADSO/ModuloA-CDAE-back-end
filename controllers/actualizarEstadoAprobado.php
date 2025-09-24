8a11d9d529268fdc408990e9a7ab551b4b748d76
Cesar Morales <191783025+CesarADSO@users.noreply.github.com>
2025-09-23 23:49:06 -0500

    Programé la funcionalidad que permite a la concesionaria presionar el botón de aceptar y rechazar, una vez se presiona el botón de aceptar, el estado de la cita cambia a aprobada. Ahora, si se presiona el botón rechazar, el estado de la cita cambia a rechazada.

---
+++ controllers/actualizarEstadoAprobado.php
@@ -0,0 +1,16 @@
+<?php
+
+// IMPORTAMOS LAS DEPENDENCIAS NECESARIAS
+require_once("../models/conexion.php");
+require_once("../models/modelCita.php");
+
+// CAPTURAMOS EN VARIABLES LOS DATOS ENVIADOS A TRAVÉS DEL FORMULARIO A TRAVÉS DEL METHOD POST Y LOS NAME DE LOS CAMPOS
+$id_cita = $_GET['idCita'];
+$estado = "APROBADA";
+
+// INSTANCIAMOS LA CLASE PRINCIPAL CITA
+$objetoCita = new Cita();
+$objetoCita->actualizarEstado($id_cita, $estado);
+
+
+?>
--- models/modelCita.php
+++ models/modelCita.php
@@ -62,7 +62,30 @@ class Cita {
         return $f;
     }
 
-    
+    public function actualizarEstado($id_cita, $estado) {
+        // CREAMOS EL OBJETO DE LA CONEXIÓN
+        $objetoConexion = new Conexion();
+        $conexion = $objetoConexion->get_conexion();
+
+        // DEFINIMOS EN UNA VARIABLE LA CONSULTA SQL A EJECUTAR
+        $actualizar = "UPDATE citas SET estado = '$estado' WHERE id = $id_cita";
+
+        // PREPARAMOS LA ACCIÓN A EJECUTAR Y LA EJECUTAMOS
+        $resultado = $conexion->prepare($actualizar);
+        $resultado->execute();
+
+        //NOTIFICAMOS Y REDIRECCIONAMOS
+        switch ($estado) {
+            case 'APROBADA':
+                echo '<script>alert("Cita Aprobada Correctamente")</script>';
+                break;
+            
+            case 'RECHAZADA':
+                echo '<script>alert("Cita Rechazada Correctamente")</script>';
+                break;
+        }
+        echo '<script>location.href="../views/conSolicitudes.php"</script>';
+    }
 }
 
 
